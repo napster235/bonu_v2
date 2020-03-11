587054ba3954022fe9ca595d87e99372428fbefe
Tudor <tudor.dragomir@sparktech.ro>
2020-03-11 20:53:25 +0200

    commented skip_action

--- app/controllers/api/v1/bons_controller.rb
+++ app/controllers/api/v1/bons_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BonsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
   def index
     @bon = Bon.all
     render json: @bon

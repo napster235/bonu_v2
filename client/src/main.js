b7cbdfc0c35e45f918c14ff9a483e6bf28fae538
denisa <denisa.popa@sparktech.ro>
2020-03-23 19:27:16 +0200

    added apollo and graphql

--- client/src/main.js
+++ client/src/main.js
@@ -5,20 +5,33 @@ import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux';
-
-
-import store from 'store';
 import Main from 'components/Main';
+
+// 1
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // Import main css file.
 import 'style.js';
 
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+// 3
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// 4
 ReactDOM.render(
-  (
-    <Provider store={store}>
-      <Main />
-    </Provider>
-  ),
-  document.querySelector('#app-container'),
+  <ApolloProvider client={client}>
+    <Main />
+  </ApolloProvider>,
+  document.getElementById('app-container'),
 );

2f3cd61ac5f1fc7593e309ed32e83f76f79884a3
Tudor <tudor.dragomir@sparktech.ro>
2020-04-12 18:45:58 +0300

    seed user and nginx configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'test', email: 'test@test.com', password: '123456')
 
 lista_bonuri = [
     ["01-02-2018", 203, "Bon Lidl"],
